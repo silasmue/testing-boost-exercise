9f12b6d6b75bd89497695bab5e2152f8dd44f394
Silas Müller <news@silas2000.de>
2026-02-03 10:31:11 +0100

    clang-format und testname fix

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -1,11 +1,11 @@
 #define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
 #include <Eigen/Dense>
+#include <boost/test/unit_test.hpp>
 #include <fstream>
 #include <string>
 #include "matrixIO.hpp"
 
-BOOST_AUTO_TEST_SUITE(MatrixIOSuite)
+BOOST_AUTO_TEST_SUITE(MatrixIOTests)
 
 BOOST_AUTO_TEST_CASE(TestCsvRead3x3)
 {
